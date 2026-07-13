8efdfe464a6841434b973ed3baeaefbabcfdc09e
taniya221 <taniya.jaison007@gmail.com>
2026-07-13 10:21:18 +0530

    home/feature added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import AddDonar from './components/AddDonar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +15,8 @@ function App() {
    <BrowserRouter>
    <Routes>
     
-    <Route path="/" element={<AddDonar />} />
+     <Route path="/" element={<Home />} />
+    <Route path="/add" element={<AddDonar />} />
    
   
    </Routes>
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavigationBar from './NavigationBar'
+
+const Home = () => {
+  return (
+    <div>
+        <NavigationBar />
+        <center>
+        <h1>Blood Bank </h1>
+           <p>Welcome to the Blood Bank Management System. Donate blood, save lives, and help those in need.</p>
+   </center>
+    </div>
+  )
+}
+
+export default Home
